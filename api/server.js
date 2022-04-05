ada367453f109aa85024509f015b04decf8d90cf
luisbryandr <luisbdominguezr@gmail.com>
2022-04-05 12:46:39 -0400

    wiring accountsRouter and establishing error handling

--- api/server.js
+++ api/server.js
@@ -1,6 +1,10 @@
 const express = require("express");
 
+const accountsRouter = require('./accounts/accounts-router');
+
 const server = express();
+
+server.use('/accounts', accountsRouter);
 
 server.use(express.json());
 
